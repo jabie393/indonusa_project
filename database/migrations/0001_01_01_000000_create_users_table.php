22568ea8a17f99fbb6b6e9cfe1d2af24a64c9333
selowers <imamularifin97@gmail.com>
2025-09-06 22:38:44 +0700

    menambahkan kolom role

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin_PT', 'adminsupply', 'adminsales', 'admin_warehouse', 'operator'])
+      ->default('operator');
             $table->rememberToken();
             $table->timestamps();
         });
