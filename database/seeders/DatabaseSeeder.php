de5b7dde7687e266f35c3438912fecfc0573cbe1
jabie393 <wahyurojabi393@gmail.com>
2025-09-06 22:55:04 +0700

    admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,7 +19,36 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' => 'password',
+            'password' => Hash::make('password'),
+            'role' => 'operator',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin PT',
+            'email' => 'adminpt@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin_PT',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin Supply',
+            'email' => 'adminsupply@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'adminsupply',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin Sales',
+            'email' => 'adminsales@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'adminsales',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin Warehouse',
+            'email' => 'adminwarehouse@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin_warehouse',
         ]);
     }
 }
