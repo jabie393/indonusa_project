fd622d1bfcf0b0c4421132dd3ea281e43cdae89d
ottoxan <ottomandora@gmail.com>
2025-09-07 17:16:07 +0700

    update login style

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,7 +17,7 @@
                     <span class="sr-only">Toggle sidebar</span>
                 </button>
                 <a href="{{ route('dashboard') }}" class="flex items-center justify-between mr-4">
-                    <img src="https://flowbite.s3.amazonaws.com/logo.svg" class="mr-3 h-8" alt="Flowbite Logo" />
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="mr-3 h-8">
                     <span class="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white">Indonusa</span>
                 </a>
                 <form action="#" method="GET" class="hidden md:block md:pl-2">
