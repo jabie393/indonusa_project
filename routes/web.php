efca91f9ae23db0ce24627624c95c330f02064be
ottoxan <ottomandora@gmail.com>
2025-09-16 18:54:14 +0700

    change footer

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,20 @@ use App\Http\Controllers\BarangController;
 use App\Http\Controllers\Guest\OrderController;
 use Illuminate\Support\Facades\Route;
 
+// Guest Routes
 Route::get('/', function () {
     return view('guest.welcome');
 });
 
 // Route khusus user untuk lihat daftar barang
 Route::get('/order', [OrderController::class, 'index'])->name('order');
+// End guest routes
 
+
+// Admin Routes
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/pagetwo', function () {
-    return view('admin.pagetwo');
-})->middleware(['auth', 'verified'])->name('pagetwo');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -26,7 +26,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/barang', [BarangController::class, function(){return view ('admin.barang.index');}])->name('barang.index');
+Route::get('/barang', [BarangController::class, function () {
+    return view('admin.barang.index');
+}])->name('barang.index');
 Route::resource('/barang', BarangController::class);
+// End Admin Routes
 
 require __DIR__ . '/auth.php';
