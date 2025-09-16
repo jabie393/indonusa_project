09f96b51b18b91f464f92726c64931c3a96d131a
jabie393 <wahyurojabi393@gmail.com>
2025-09-17 04:52:49 +0700

    edit

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -52,8 +52,6 @@
         </div>
         <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        @vite('node_modules/flowbite/dist/flowbite.min.js')
-        @vite('resources/js/ajax.js')
     </body>
 
 </html>
