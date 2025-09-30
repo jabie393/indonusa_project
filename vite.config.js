4e84e2295df1036f3cc5a139dd833b2cc9ee481e
ottoxan <ottomandora@gmail.com>
2025-09-30 20:09:07 +0700

    added js in vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'node_modules/flowbite/dist/flowbite.min.js', 'resources/js/add-stock.js', 'resources/js/sweetalert.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'node_modules/flowbite/dist/flowbite.min.js',
+                'resources/js/add-stock.js',
+                'resources/js/sweetalert.js',
+                'resources/js/warehouse.js',
+            ],
             refresh: true,
         }),
     ],
