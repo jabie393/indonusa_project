65ec34c475b88743c66531813d72865700402d84
ottoxan <ottomandora@gmail.com>
2025-10-05 01:14:41 +0700

    Enhance the add-stock-edit modal UI with a gradient header, improved layout, and updated styling for item details and stock update form.

--- resources/views/admin/goods-in/index.blade.php
+++ resources/views/admin/goods-in/index.blade.php
@@ -1,32 +1,49 @@
 <x-app-layout>
     <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
-        <div class="flex flex-col items-center justify-between space-y-3 p-4 md:flex-row md:space-x-4 md:space-y-0">
+        <div class="flex flex-col items-center justify-center space-y-3 p-4 md:flex-row md:space-x-4 md:space-y-0">
             <div class="flex w-full flex-shrink-0 flex-col items-stretch justify-end space-y-2 md:w-auto md:flex-row md:items-center md:space-x-3 md:space-y-0">
                 <div x-data="{ tambahModalIsOpen: false }">
                     <button
-                        class="btn flex items-center justify-center rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        class="flex h-52 w-52 flex-col items-center justify-evenly rounded-lg bg-[#225A97] px-4 py-2 text-sm font-medium text-white hover:bg-[#2868b3] focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                         onclick="tambahBarang.showModal()">
-                        <svg class="mr-2 h-3.5 w-3.5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="97" height="86" viewBox="0 0 97 86" fill="none">
+                            <g clip-path="url(#clip0_377_33)">
+                                <path
+                                    d="M88.9166 10.75H8.08325V32.25H12.1249V71.6667C12.1249 75.6263 15.7422 78.8333 20.2083 78.8333H76.7916C81.2576 78.8333 84.8749 75.6263 84.8749 71.6667V32.25H88.9166V10.75ZM16.1666 17.9167H80.8333V25.0833H16.1666V17.9167ZM76.7916 71.6667H20.2083V32.25H76.7916V71.6667ZM52.5416 35.8333V54.3592L63.0095 45.1142L68.7082 50.1667L48.4999 68.0833L28.2916 50.1667L33.9903 45.0783L44.4583 54.3592V35.8333H52.5416Z"
+                                    fill="white" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_377_33">
+                                    <rect width="97" height="86" fill="white" />
+                                </clipPath>
+                            </defs>
                         </svg>
-                        Tambah Barang
+                        <div class="flex items-center">
+                            <svg class="mr-2 h-3.5 w-3.5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                            </svg>
+                            Tambah Barang
+                        </div>
                     </button>
                 </div>
                 <a href="{{ route('add-stock.index') }}"
-                    class="flex items-center justify-center rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    <svg class="mr-2 h-3.5 w-3.5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                    class="flex h-52 w-52 flex-col items-center justify-evenly rounded-lg bg-[#225A97] px-4 py-2 text-sm font-medium text-white hover:bg-[#2868b3] focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="85" height="79" viewBox="0 0 85 79" fill="none">
+                        <path
+                            d="M73.8847 29.7769C74.535 29.778 75.1616 30.0043 75.6422 30.4116C76.1227 30.8189 76.4227 31.378 76.4837 31.9798V66.2385C76.4776 68.1163 75.6956 69.9201 74.2993 71.2773C72.9029 72.6344 70.9989 73.4412 68.9808 73.5308H16.3462C14.3258 73.5251 12.3849 72.7983 10.9247 71.5005C9.46446 70.2027 8.59646 68.4332 8.50006 66.5575V32.2077C8.50126 31.6032 8.74473 31.0209 9.18296 30.5743C9.62119 30.1276 10.2227 29.8488 10.8702 29.7921H73.8847V29.7769ZM53.6318 39.9102L53.452 40.0317L38.4789 55.1177L31.6952 49.0408C31.3511 48.7205 30.8911 48.5306 30.4046 48.5082C29.9182 48.4857 29.4401 48.6322 29.0635 48.9192L28.9001 49.0408L26.1539 51.35C25.9858 51.4823 25.8482 51.6448 25.7493 51.8278C25.6505 52.0107 25.5924 52.2104 25.5787 52.4147C25.565 52.619 25.5958 52.8239 25.6694 53.0168C25.7429 53.2098 25.8577 53.387 26.0068 53.5377L26.1539 53.6896L35.7327 62.1214C36.4788 62.8041 37.481 63.1909 38.5279 63.2C39.0492 63.2129 39.5676 63.1234 40.0492 62.9376C40.5309 62.7517 40.9649 62.4736 41.3231 62.1214L49.3327 54.2821L49.9702 53.6592L50.8203 52.8237L58.9933 44.7717C59.2741 44.4652 59.4428 44.0838 59.4751 43.6824C59.5075 43.281 59.4017 42.8804 59.1731 42.5385L59.0587 42.4017L56.2472 40.0621C55.91 39.7385 55.4549 39.5435 54.9711 39.5154C54.4874 39.4873 54.0097 39.6281 53.6318 39.9102ZM73.8847 5.46924C75.9643 5.47325 77.9575 6.24284 79.428 7.60954C80.8985 8.97624 81.7265 10.8287 81.7308 12.7615V20.0539C81.7308 20.6985 81.4553 21.3168 80.9648 21.7727C80.4743 22.2285 79.8091 22.4846 79.1154 22.4846H5.88467C5.19103 22.4846 4.5258 22.2285 4.03532 21.7727C3.54484 21.3168 3.26929 20.6985 3.26929 20.0539V12.7615C3.27361 10.8287 4.10164 8.97624 5.57214 7.60954C7.04265 6.24284 9.03584 5.47325 11.1154 5.46924H73.8847Z"
+                            fill="white" />
                     </svg>
-                    Tambah Stok Barang
+                    <div class="flex items-center">
+                        <svg class="mr-2 h-3.5 w-3.5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                        </svg>
+                        Tambah Stok Barang
+                    </div>
                 </a>
             </div>
         </div>
-
-
-    </div>
     </div>
 
-    </div>
 
     <!-- Modals -->
     @include('components.goods-in-modal-tambah')
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,17 +1,12 @@
 <!-- Sidebar -->
-<aside
-    class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full pt-14 transition-transform dark:bg-[#0B1D31] md:translate-x-0"
-    aria-label="Sidenav" id="drawer-navigation">
-    <div
-        class="h-full overflow-y-auto bg-gradient-to-r from-[#225A97] to-[#1F5188] px-3 py-5 dark:bg-gradient-to-r dark:from-[#0B1D31] dark:to-[#0E243D]">
+<aside class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full pt-14 transition-transform dark:bg-[#0B1D31] md:translate-x-0" aria-label="Sidenav" id="drawer-navigation">
+    <div class="h-full overflow-y-auto bg-gradient-to-r from-[#225A97] to-[#1F5188] px-3 py-5 dark:bg-gradient-to-r dark:from-[#0B1D31] dark:to-[#0E243D]">
         <form action="#" method="GET" class="mb-2 md:hidden">
             <label for="sidebar-search" class="sr-only">Search</label>
             <div class="relative">
                 <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg class="h-5 w-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z">
+                    <svg class="h-5 w-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z">
                         </path>
                     </svg>
                 </div>
@@ -43,22 +38,20 @@
                 <li>
                     <a href="{{ route('goods-in.index') }}"
                         class="{{ request()->routeIs('goods-in.*') ? 'bg-gray-200 dark:bg-gray-700' : '' }} group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <svg aria-hidden="true"
-                            class="{{ request()->routeIs('goods-in.*') ? 'text-black dark:text-white' : 'text-white' }} h-6 w-6 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white"
-                            fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M4 4h12v12H4z"></path>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 27 27" fill="none" class="{{ request()->routeIs('goods-in.*') ? 'text-black dark:text-white' : 'text-white' }} group-hover:text-black dark:group-hover:text-white">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M13.5 0.0795898L26.625 6.79129V20.2147L13.5 26.9264L0.375 20.2147V6.79129L13.5 0.0795898ZM9.12493 12.6078L9.125 21.7063L12.0417 23.1978V14.0993L9.12493 12.6078ZM3.29167 9.62501V18.7232L6.20831 20.2148V11.1164L3.29167 9.62501ZM19.2416 5.99859L10.6571 10.4086L13.5 11.8623L22.1042 7.46246L19.2416 5.99859ZM13.5 3.06257L4.89583 7.46246L7.7438 8.91877L16.3283 4.50884L13.5 3.06257Z"
+                                fill="currentColor" />
                         </svg>
-                        <span
-                            class="{{ request()->routeIs('goods-in.*') ? 'text-black dark:text-white' : 'text-white' }} ml-3 group-hover:text-black dark:group-hover:text-white">Goods
+                        <span class="{{ request()->routeIs('goods-in.*') ? 'text-black dark:text-white' : 'text-white' }} ml-3 group-hover:text-black dark:group-hover:text-white">Goods
                             In</span>
                     </a>
                     {{-- Add Stock --}}
                     <ul>
                         @if (request()->routeIs('add-stock.*'))
                             <li class="flex items-center justify-end">
-                                <svg width="64px"
-                                    class="h-6 w-6 text-white transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white"
-                                    height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <svg width="64px" class="h-6 w-6 text-white transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white" height="64px" viewBox="0 0 24 24"
+                                    fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                                     <g id="SVGRepo_iconCarrier">
@@ -84,10 +77,14 @@
                 <li>
                     <a href="{{ route('goods-in-status.index') }}"
                         class="{{ request()->routeIs('goods-in-status.*') ? 'bg-gray-200 dark:bg-gray-700' : '' }} group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                        <svg aria-hidden="true"
-                            class="{{ request()->routeIs('goods-in-status.*') ? 'text-black dark:text-white' : 'text-white' }} h-6 w-6 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white"
-                            fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M4 4h12v12H4z"></path>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="29" viewBox="0 0 28 29" fill="none">
+                            <path d="M8.20312 13.0952H19.8187V14.4546H8.20312V13.0952ZM8.20312 19.0765H15.3125V20.4358H8.20312V19.0765Z" fill="currentColor"  class="{{ request()->routeIs('goods-in-status.*') ? 'text-black dark:text-white' : 'text-white dark:text-black' }} group-hover:text-black dark:text-white dark:group-hover:text-white"/>
+                            <path
+                                d="M22.3781 27.7992H5.62188C5.27188 27.7992 4.94375 27.7313 4.61563 27.5953C4.30938 27.4594 4.025 27.2781 3.80625 27.0289C3.56562 26.7797 3.39062 26.5078 3.25937 26.1906C3.12812 25.8508 3.0625 25.5109 3.0625 25.1484V7.09141C3.0625 6.72891 3.12812 6.38906 3.25937 6.04922C3.39062 5.73203 3.56562 5.4375 3.80625 5.21094C4.04687 4.96172 4.30938 4.78047 4.61563 4.64453C4.94375 4.50859 5.27188 4.44063 5.62188 4.44063H8.15937V5.77734H5.62188C4.92188 5.77734 4.33125 6.36641 4.33125 7.11406V25.1711C4.33125 25.8961 4.9 26.5078 5.62188 26.5078H22.3562C23.0562 26.5078 23.6469 25.9188 23.6469 25.1711V7.09141C23.6469 6.36641 23.0781 5.75469 22.3562 5.75469H19.8406V4.41797H22.3781C22.7281 4.41797 23.0562 4.48594 23.3844 4.62187C23.6906 4.75781 23.975 4.93906 24.1938 5.18828C24.4344 5.4375 24.6094 5.70938 24.7406 6.02656C24.8719 6.36641 24.9375 6.70625 24.9375 7.06875V25.1258C24.9375 25.4883 24.8719 25.8281 24.7406 26.168C24.6094 26.4852 24.4344 26.7797 24.1938 27.0063C23.9531 27.2555 23.6906 27.4367 23.3844 27.5727C23.0562 27.7313 22.7281 27.7992 22.3781 27.7992Z"
+                                fill="currentColor" />
+                            <path
+                                d="M20.4313 8.42822H7.56885V4.07822C7.56885 3.14932 8.29072 2.40166 9.1876 2.40166H10.8501C11.4407 1.17822 12.6657 0.385254 14.022 0.385254C15.3782 0.385254 16.6032 1.17822 17.1938 2.40166H18.8563C19.7532 2.40166 20.4751 3.14932 20.4751 4.07822L20.4313 8.42822ZM8.85947 7.0915H19.1626V4.07822C19.1626 3.89697 19.0095 3.73838 18.8345 3.73838H16.2751L16.122 3.28525C15.8157 2.35635 14.9626 1.72197 14.0001 1.72197C13.0376 1.72197 12.1845 2.35635 11.8782 3.28525L11.7251 3.73838H9.16572C8.99072 3.73838 8.8376 3.89697 8.8376 4.07822V7.0915H8.85947Z"
+                                fill="currentColor" />
                         </svg>
                         <span
                             class="{{ request()->routeIs('goods-in-status.*') ? 'text-black dark:text-white' : 'text-white dark:text-black' }} ml-3 group-hover:text-black dark:text-white dark:group-hover:text-white">Goods
@@ -98,10 +95,17 @@
                 <li>
                     <a href="{{ route('history.index') }}"
                         class="{{ request()->routeIs('history.*') ? 'bg-gray-200 dark:bg-gray-700' : '' }} group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                        <svg aria-hidden="true"
-                            class="{{ request()->routeIs('history.*') ? 'text-black dark:text-white' : 'text-white' }} h-6 w-6 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white"
-                            fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M4 4h12v12H4z"></path>
+                        <svg width="28px" height="28px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="{{ request()->routeIs('history.*') ? 'text-black dark:text-white' : 'text-white dark:text-black' }} group-hover:text-black dark:text-white dark:group-hover:text-white">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M3 5.67541V3C3 2.44772 2.55228 2 2 2C1.44772 2 1 2.44772 1 3V7C1 8.10457 1.89543 9 3 9H7C7.55229 9 8 8.55229 8 8C8 7.44772 7.55229 7 7 7H4.52186C4.54218 6.97505 4.56157 6.94914 4.57995 6.92229C5.621 5.40094 7.11009 4.22911 8.85191 3.57803C10.9074 2.80968 13.173 2.8196 15.2217 3.6059C17.2704 4.3922 18.9608 5.90061 19.9745 7.8469C20.9881 9.79319 21.2549 12.043 20.7247 14.1724C20.1945 16.3018 18.9039 18.1638 17.0959 19.4075C15.288 20.6513 13.0876 21.1909 10.9094 20.9247C8.73119 20.6586 6.72551 19.605 5.27028 17.9625C4.03713 16.5706 3.27139 14.8374 3.06527 13.0055C3.00352 12.4566 2.55674 12.0079 2.00446 12.0084C1.45217 12.0088 0.995668 12.4579 1.04626 13.0078C1.25994 15.3309 2.2082 17.5356 3.76666 19.2946C5.54703 21.3041 8.00084 22.5931 10.6657 22.9188C13.3306 23.2444 16.0226 22.5842 18.2345 21.0626C20.4464 19.541 22.0254 17.263 22.6741 14.6578C23.3228 12.0526 22.9963 9.30013 21.7562 6.91897C20.5161 4.53782 18.448 2.69239 15.9415 1.73041C13.4351 0.768419 10.6633 0.756291 8.14853 1.69631C6.06062 2.47676 4.26953 3.86881 3 5.67541Z"
+                                    fill="currentColor"></path>
+                                <path
+                                    d="M12 5C11.4477 5 11 5.44771 11 6V12.4667C11 12.4667 11 12.7274 11.1267 12.9235C11.2115 13.0898 11.3437 13.2344 11.5174 13.3346L16.1372 16.0019C16.6155 16.278 17.2271 16.1141 17.5032 15.6358C17.7793 15.1575 17.6155 14.546 17.1372 14.2698L13 11.8812V6C13 5.44772 12.5523 5 12 5Z"
+                                    fill="currentColor"></path>
+                            </g>
                         </svg>
                         <span
                             class="{{ request()->routeIs('history.*') ? 'text-black dark:text-white' : 'text-white dark:text-black' }} ml-3 group-hover:text-black dark:text-white dark:group-hover:text-white">History</span>
@@ -120,8 +124,7 @@
                             fill="currentColor" viewBox="0 0 20 20">
                             <path d="M4 4h12v12H4z"></path>
                         </svg>
-                        <span
-                            class="{{ request()->routeIs('supply-orders.*') ? 'text-black dark:text-white' : 'text-white' }} ml-3 group-hover:text-black">Supply
+                        <span class="{{ request()->routeIs('supply-orders.*') ? 'text-black dark:text-white' : 'text-white' }} ml-3 group-hover:text-black">Supply
                             Orders</span>
                     </a>
                 </li>
@@ -131,47 +134,45 @@
             <li>
                 <a href="{{ route('warehouse.index') }}"
                     class="{{ request()->routeIs('warehouse.*') ? 'bg-gray-200 dark:bg-gray-700' : '' }} group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                    <svg aria-hidden="true"
-                        class="{{ request()->routeIs('warehouse.*') ? 'text-black dark:text-white' : 'text-white' }} h-6 w-6 transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white"
-                        fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M4 4h12v12H4z"></path>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="31" viewBox="0 0 26 31" fill="none"
+                        class="{{ request()->routeIs('warehouse.*') ? 'text-black dark:text-white' : 'text-white' }} group-hover:text-black dark:group-hover:text-white">
+                        <path
+                            d="M6.21582 14.4103H10.0143V18.1884H6.21582V14.4103ZM6.21582 23.2138H10.0143V19.4356H6.21582V23.2138ZM6.21582 28.227H10.0143V24.4489H6.21582V28.227ZM11.1416 23.2138H14.9299V19.4356H11.1314V23.2138H11.1416ZM11.1416 28.227H14.9299V24.4489H11.1314V28.227H11.1416ZM16.0674 24.4489V28.227H19.8658V24.4489H16.0674ZM25.8986 9.71182L13.0307 2.49463L0.172852 9.73603L0.975195 11.7341L2.61035 10.8017V28.2392H4.45879V10.729H21.6229V28.2634H23.4713V10.8259L25.1064 11.7341L25.8986 9.71182Z"
+                            fill="currentColor" />
                     </svg>
-                    <span
-                        class="{{ request()->routeIs('warehouse.*') ? 'text-black dark:text-white' : 'text-white' }} ml-3 group-hover:text-black dark:group-hover:text-white">Warehouse</span>
+                    <span class="{{ request()->routeIs('warehouse.*') ? 'text-black dark:text-white' : 'text-white' }} ml-3 group-hover:text-black dark:group-hover:text-white">Warehouse</span>
                 </a>
             </li>
 
             {{-- Menu untuk admin_supply --}}
             @if (in_array(auth()->user()->role, ['admin_PT']))
-                        {{-- Incoming Orders --}}
-                        <li>
-                            <a href="{{ route('orders.incoming') }}"
-                                class="{{ request()->routeIs('orders.incoming') ? 'bg-gray-200 dark:bg-gray-700' : '' }} group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                <svg aria-hidden="true"
-                                    class="h-6 w-6 {{ request()->routeIs('orders.incoming') ? 'text-black dark:text-white' : 'text-white' }}  transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                                    fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M3 3h14v2H3V3zm0 4h14v10H3V7z"></path>
-                                </svg>
-                                <span
-                                    class="ml-3 {{ request()->routeIs('orders.incoming') ? 'text-black dark:text-white' : 'text-white' }} group-hover:text-black dark:group-hover:text-white">Incoming
-                                    Orders</span>
-                            </a>
-                        </li>
+                {{-- Incoming Orders --}}
+                <li>
+                    <a href="{{ route('orders.incoming') }}"
+                        class="{{ request()->routeIs('orders.incoming') ? 'bg-gray-200 dark:bg-gray-700' : '' }} group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                        <svg aria-hidden="true"
+                            class="{{ request()->routeIs('orders.incoming') ? 'text-black dark:text-white' : 'text-white' }} h-6 w-6 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                            fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M3 3h14v2H3V3zm0 4h14v10H3V7z"></path>
+                        </svg>
+                        <span class="{{ request()->routeIs('orders.incoming') ? 'text-black dark:text-white' : 'text-white' }} ml-3 group-hover:text-black dark:group-hover:text-white">Incoming
+                            Orders</span>
+                    </a>
+                </li>
 
 
-                        {{-- History Orders --}}
-                        <li>
-                            <a href="{{ route('orders.history') }}"
-                                class="{{ request()->routeIs('admin.orders.history') ? 'bg-gray-200 dark:bg-gray-700' : '' }} group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                <svg aria-hidden="true"
-                                    class="h-6 w-6 text-white transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                                    fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M5 4h10v2H5V4zM5 8h10v2H5V8zM5 12h6v2H5v-2z"></path>
-                                </svg>
-                                <span class="ml-3 ">History Orders</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            @endif
+                {{-- History Orders --}}
+                <li>
+                    <a href="{{ route('orders.history') }}"
+                        class="{{ request()->routeIs('admin.orders.history') ? 'bg-gray-200 dark:bg-gray-700' : '' }} group flex items-center rounded-lg p-2 text-base font-medium text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                        <svg aria-hidden="true" class="h-6 w-6 text-white transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor"
+                            viewBox="0 0 20 20">
+                            <path d="M5 4h10v2H5V4zM5 8h10v2H5V8zM5 12h6v2H5v-2z"></path>
+                        </svg>
+                        <span class="ml-3">History Orders</span>
+                    </a>
+                </li>
+        </ul>
+    </div>
+    @endif
 </aside>
