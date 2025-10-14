c3a05aae541581147110199066da5ca631879a64
ottoxan <ottomandora@gmail.com>
2025-10-14 19:24:38 +0700

    fix warehouse datatables

--- resources/views/components/supply-orders-modal-reject.blade.php
+++ resources/views/components/supply-orders-modal-reject.blade.php
@@ -4,7 +4,7 @@
         <input type="hidden" name="id" id="reject_barang_id">
         <div class="mb-4">
             <label for="reject_note" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alasan Penolakan</label>
-            <textarea name="catatan" id="reject_note" rows="3" required class="block w-full rounded-lg border border-gray-300 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-800 dark:text-white"></textarea>
+            <textarea name="catatan" id="reject_note" rows="3" required class="block w-full rounded-lg border bg-white border-gray-300 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-800 dark:text-white"></textarea>
         </div>
         <div class="flex justify-end gap-2">
             <button type="button" id="closeRejectNoteModal" class="rounded bg-gray-400 px-4 py-2 text-white">Cancel</button>
