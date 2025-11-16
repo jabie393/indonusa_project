22e700e27baed80207cbfcc96c1d100724c1f533
ottoxan <ottomandora@gmail.com>
2025-11-16 21:50:46 +0700

    p

--- resources/views/admin/akun-sales/index.blade.php
+++ resources/views/admin/akun-sales/index.blade.php
@@ -22,6 +22,7 @@
                         </div>
                     </form>
                 </div>
+                
                 <div class="p-4">
                     <button onclick="createUserModal.showModal()"
                         class="flex items-center justify-center rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
