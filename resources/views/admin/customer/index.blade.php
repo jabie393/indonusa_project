248c1d50aacb193c3c96be32a37662b010805776
ottoxan <ottomandora@gmail.com>
2025-12-10 18:38:17 +0700

    style for select2 pic

--- resources/views/admin/customer/index.blade.php
+++ resources/views/admin/customer/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-    <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+    <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg inset-shadow-none dark:inset-shadow-gray-500 dark:inset-shadow-sm">
         <div
-            class="flex flex-col items-center justify-between space-y-3 bg-gradient-to-r from-[#225A97] to-[#0D223A] p-4 md:flex-row md:space-x-4 md:space-y-0">
+            class="flex flex-col items-center justify-between space-y-3 bg-gradient-to-r from-[#225A97] to-[#0D223A] p-4 md:flex-row md:space-x-4 md:space-y-0 inset-shadow-none dark:inset-shadow-gray-500 dark:inset-shadow-sm">
             <div>
                 <h2 class="mr-3 font-semibold text-white">Kelola Customer</h2>
             </div>
