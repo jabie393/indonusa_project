2127bea1203520d2f094f093631cfb74b8ac66ba
selowers <imamularifin97@gmail.com>
2025-12-10 18:14:23 +0700

    fix: adjust spacing in header for sent penawaran view

--- resources/views/admin/sent-penawaran/sent_penawaran.blade.php
+++ resources/views/admin/sent-penawaran/sent_penawaran.blade.php
@@ -2,7 +2,7 @@
     <div class="relative overflow-hidden rounded-2xl bg-white shadow-md dark:bg-gray-800">
         <div class="flex flex-col items-center justify-between space-y-3 bg-gradient-to-r from-[#225A97] to-[#0D223A] p-4 md:flex-row md:space-x-4 md:space-y-0">
             <div>
-                <h2 class="mr-3 font-semibold text-white">Sent Penawaran (Waiting Approval)</h2>
+                <h2 class="mr-3 font-semibold text-white"> Sent Penawaran (Waiting Approval)</h2>
             </div>
 
             @if (session('success'))
